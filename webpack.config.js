67636a3bfe077463fbdb7bc01ba86b4a5a272f14
Kyle Wilcox <kyle@axiomdatascience.com>
2018-10-01 13:23:29 -0400

    Adding Quick Link to ARGOS help docs

--- webpack.config.js
+++ webpack.config.js
@@ -129,6 +129,7 @@ const config = {
     }),
     new CopyWebpackPlugin([{from: 'images/*'}]),
     new CopyWebpackPlugin([{from: 'src/help/_images/*', to: 'help/_images/', flatten: true}]),
+    new CopyWebpackPlugin([{from: 'src/help/_downloads/*', to: 'help/_downloads/', flatten: true}]),
   ],
 };
 
